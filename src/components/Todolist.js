5916889f63e28abfce1083aec1bc939c29446e97
mh37 <78908148+mh37@users.noreply.github.com>
2022-02-14 09:50:33 +0200

    simple todolist complete

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Todolist(){
+
+    const [description, setDescription] = useState('');
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = () => {
+        setTodos([description, ...todos]);
+        setDescription('');
+    }
+
     return(
         <div>
-            Todos
+            <h1>Todolist</h1>
+            <input 
+                placeholder='Description' 
+                type="text" 
+                value={description}
+                onChange={event => setDescription(event.target.value)}
+            />
+            <button onClick={addTodo}>Add</button>
+            <table>
+                <tbody>
+                    {
+                        todos.map((todo, index) =>
+                            <tr key={index}> 
+                                <td>{todo}</td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+
+            </table>
         </div>
     );
 }
