5a7170cc25197f2481d90c254bc5071d49f7f326
mh37 <78908148+mh37@users.noreply.github.com>
2022-05-05 15:42:25 +0300

    improvements on the calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,24 +1,91 @@
-//https://code.daypilot.org/42221/react-weekly-calendar-tutorial
-
 import React, {Component} from 'react';
-import {DayPilot, DayPilotCalendar} from "@daypilot/daypilot-lite-react";
+import {DayPilot, DayPilotCalendar, DayPilotNavigator} from "@daypilot/daypilot-lite-react";
+
+
+const styles = {
+  wrap: {
+    display: "flex"
+  },
+  left: {
+    marginRight: "10px"
+  },
+  main: {
+    flexGrow: "1"
+  }
+};
 
 class Calendar extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      viewType: "Week"
+      viewType: "Week",
+      durationBarVisible: false
     };
   }
+
+  componentDidMount() {
+
+    // load event data
+    this.setState({
+      startDate: "2022-03-07",
+      events: [
+        {
+          id: 1,
+          text: "Event 1",
+          start: "2022-03-07T10:30:00",
+          end: "2022-03-07T13:00:00"
+        },
+        {
+          id: 2,
+          text: "Event 2",
+          start: "2022-03-08T09:30:00",
+          end: "2022-03-08T11:30:00",
+          backColor: "#6aa84f"
+        },
+        {
+          id: 3,
+          text: "Event 3",
+          start: "2022-03-08T12:00:00",
+          end: "2022-03-08T15:00:00",
+          backColor: "#f1c232"
+        },
+        {
+          id: 4,
+          text: "Event 4",
+          start: "2022-03-06T11:30:00",
+          end: "2022-03-06T14:30:00",
+          backColor: "#cc4125"
+        },
+      ]
+    });
+  }
+
 
   render() {
     const {...config} = this.state;
     return (
-      <div>
+      <div style={styles.wrap}>
+        <div style={styles.left}>
+          <DayPilotNavigator
+            selectMode={"week"}
+            showMonths={2}
+            skipMonths={2}
+            onTimeRangeSelected={ args => {
+              this.setState({
+                startDate: args.day
+              });
+            }}
+          />
+        </div>
+        <div style={styles.main}>
         <DayPilotCalendar
           {...config}
+          ref={component => {
+            this.calendar = component && component.control;
+          }}
         />
+        </div>
       </div>
     );
   }
