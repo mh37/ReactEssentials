2ca80dcd8d6447cdcefcf0bee36d9abdb6a363e7
mh37 <78908148+mh37@users.noreply.github.com>
2022-05-05 14:59:45 +0300

    add calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,10 +1,27 @@
-function Calendar()   {
+//https://code.daypilot.org/42221/react-weekly-calendar-tutorial
 
-    return ( 
-        <>
+import React, {Component} from 'react';
+import {DayPilot, DayPilotCalendar} from "@daypilot/daypilot-lite-react";
 
-        </>
+class Calendar extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewType: "Week"
+    };
+  }
+
+  render() {
+    const {...config} = this.state;
+    return (
+      <div>
+        <DayPilotCalendar
+          {...config}
+        />
+      </div>
     );
+  }
 }
 
 export default Calendar;
