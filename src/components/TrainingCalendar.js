4d957412d7c64752cd9ddadd29e14043b103a992
mh37 <78908148+mh37@users.noreply.github.com>
2022-05-05 17:45:28 +0300

    calendar implementation completed

--- src/components/TrainingCalendar.js
+++ src/components/TrainingCalendar.js
@@ -3,27 +3,41 @@ import format from 'date-fns/format'
 import parse from 'date-fns/parse'
 import startOfWeek from 'date-fns/startOfWeek'
 import getDay from 'date-fns/getDay'
-import enUS from 'date-fns/locale/en-US'
+import enGB from 'date-fns/locale/en-GB'
+import React, {useState, useEffect} from 'react';
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
 
 function TrainingCalendar(){
 
+  useEffect(() => {
+    fetchTrainings()
+  });
+
   const locales = {
-    'en-US': enUS,
+    'en-GB': enGB,
   }
 
-  const eventsList = [
-    {
-      start: moment().toDate(),
-      end: moment()
-        .add(1, "days")
-        .toDate(),
-      title: "Some title"
+  const [trainings, setTrainings] = useState([]);
+
+  //Simplified fetch of trainings with custom info 
+  const fetchTrainings = () => {
+    fetch("https://customerrest.herokuapp.com/gettrainings")
+    .then(response => response.json())
+    .then(data => setTrainings(data))
+    .catch(err => console.log(err))
+  } 
+
+  const eventList = trainings.map((training)=>{
+    const endDate = moment(training.date).add(training.duration, 'minutes');
+    return {
+      title: training.activity + " / " + training.customer.firstname + " " + training.customer.lastname,
+      start: new Date(training.date),
+      end: new Date(endDate) 
     }
-  ];
-  
+  })
+
   const localizer = dateFnsLocalizer({
     format,
     parse,
@@ -37,7 +51,7 @@ function TrainingCalendar(){
         localizer={localizer}
         defaultDate={new Date()}
         defaultView="month"
-        events={eventsList}
+        events={eventList}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 500 }}
